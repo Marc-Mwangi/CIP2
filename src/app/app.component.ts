ede98b16cd5dd67b0433bfe9fc286ce8a171556e
MarcMwangi <marc.mwangi@student.moringaschool.com>
2022-01-18 02:35:39 +0300

    Creating class component for user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  user: String[]=[];
+
+  getUser(){
+
+  }
+
+
 }
